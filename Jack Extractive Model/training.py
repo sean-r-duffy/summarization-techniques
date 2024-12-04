875a625dea9af183a95c5f8ebd12442975de41fe
jcarpini <jackcarpini@gmail.com>
2024-12-03 23:15:12 -0500

    govrep training run

--- Jack Extractive Model/training.py
+++ Jack Extractive Model/training.py
@@ -17,15 +17,18 @@ print("PACKAGES SUCCESS")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-# load data (arxiv)
+# # load data (arxiv)
 # ds = load_dataset("ccdv/arxiv-summarization", "section")
 # ds_short = DatasetDict({"train": ds["train"].select(range(100)),
 #             "validation": ds["validation"].select(range(1)),
 #             "test": ds["test"].select(range(1))})
 
-# load data pubmed
-print("pubmed data")
-ds = load_dataset("ccdv/pubmed-summarization", "section")
+# # load data pubmed
+# print("pubmed data")
+# ds = load_dataset("ccdv/pubmed-summarization", "section")
+
+# load data govreport (MUST CHANGE ARTICLE AND ABSTRACT TO FIT)
+ds = load_dataset("ccdv/govreport-summarization")
 
 
 # Create Dataset and DataLoader
@@ -70,4 +73,4 @@ for epoch in range(num_epochs):
 with open("training_results.json", "w") as outfile: 
     json.dump(training_dict, outfile)
 
-torch.save(scoring_model.state_dict(), "pubmed_relevance_scoring_model.pt")
+torch.save(scoring_model.state_dict(), "govrep_relevance_scoring_model.pt")
